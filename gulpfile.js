efc8c56292e8424acd101c0174496a5c60948e82
edarioq <edarioq@gmail.com>
2017-07-03 10:21:17 -0500

    Optimized gulp tasks, add dist and app folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,82 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var minify      = require('gulp-minify');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-gulp.task('serve', function() {
+/*
+ * Development tasks
+ */
 
-    browserSync.init({
-        proxy: "localhost/neighborhood-map/"
-    });
+// Run a server and point to working directory
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  }, function() {
+    console.log('Running local server on port 3000');
+  })
+})
 
-    gulp.watch("sass/*.scss", ['sass']);
-    gulp.watch("js/*.js", ['compress']);
-    gulp.watch("js/**/*.js").on('change', browserSync.reload);
-    gulp.watch("*.html").on('change', browserSync.reload);
-});
-
-gulp.task('compress', function () {
-  gulp.src('js/main.js')
-    .pipe(minify ({
-      ext: {
-        min: '.min.js',
-      },
-      ignoreFiles: ['.min.js'],
-    }))
-    .pipe(gulp.dest('js'))
-    .pipe(browserSync.stream());
-});
-
+// Compiles SASS to CSS and watch for changes
 gulp.task('sass', function() {
-    return gulp.src("sass/*.scss")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream());
+  return gulp.src('app/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('default', ['serve']);
+// Watch for changes and reload browser
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload);  
+})
+
+/*
+ * Production ready tasks
+ */
+
+// Clean up files not being used in dist folder
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
+// Send production rdy HTML, CSS, and JS to dist folder
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+// Send fonts to dist folder
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+// Run: Development environment
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync'], 'watch',
+    callback
+  )
+})
+
+// Run: Distribution folder
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    'sass',
+    ['useref', 'fonts'],
+    callback
+  )
+})
