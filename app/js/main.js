8f97b7d2085c337d30a7a0a88f1ca34ba594dd57
edarioq <edarioq@gmail.com>
2017-08-05 18:39:30 -0500

    Add first KO binding

--- app/js/main.js
+++ app/js/main.js
@@ -4,36 +4,31 @@
 
 var locations = [
     {
-        id: 'siembra',
         name: 'Siembra Coworking',
         lat: 6.2104954,
         long: -75.5677766,
     },
     {   
-        id: 'entrevinetas',
         name: 'Entreviñetas',
         lat: 6.2095819,
         long: -75.5711447,
     },
     {   
-        id: 'tinkko',
         name: 'Tinkko Coworking',
         lat: 6.2126192,
         long: -75.5722686,
     },
     {
-        id: 'atomHouse',
         name: 'AtomHouse',
         lat: 6.209196,
         long: -75.573853,
     },
     {
-        id: 'nodo',
         name: 'NODO Coworking - Astorga',
         lat: 6.2094639,
         long: -75.5763566,
     },
-]
+];
 
 /*
  * View
@@ -49,13 +44,28 @@ function initMap() {
         scrollwheel: false
     });
 
-    // Show markers in GMaps
+    
+
+}   
+
+var locationData = function() {
     for (var i = 0; i < locations.length; i++) {
-         var marker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
             position: {lat: locations[i].lat, lng: locations[i].long},
             map: map,
-            title: locations[i].name,
+            title: 'Hello World!'
         });
     }
 }
 
+var viewModel = function() {
+    var self = this;
+
+    self.filterLocation = ko.observable("");
+
+    var locationsArray = ko.observableArray(locations);
+
+    console.log(locationsArray());
+};
+
+ko.applyBindings(viewModel);
