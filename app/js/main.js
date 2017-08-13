43dccd569e7ee6ab934639d01dc013893d049178
edarioq <edarioq@gmail.com>
2017-08-13 12:38:25 -0500

    Add KO bindings to search bar, list view, and markers

--- app/js/main.js
+++ app/js/main.js
@@ -1,71 +1,158 @@
 /*
- * Model
+ * Model (Data)
  */
-
 var locations = [
     {
         name: 'Siembra Coworking',
         lat: 6.2104954,
-        long: -75.5677766,
+        lng: -75.5677766,
     },
     {   
         name: 'Entreviñetas',
         lat: 6.2095819,
-        long: -75.5711447,
+        lng: -75.5711447,
     },
     {   
         name: 'Tinkko Coworking',
         lat: 6.2126192,
-        long: -75.5722686,
+        lng: -75.5722686,
     },
     {
         name: 'AtomHouse',
         lat: 6.209196,
-        long: -75.573853,
+        lng: -75.573853,
     },
     {
         name: 'NODO Coworking - Astorga',
         lat: 6.2094639,
-        long: -75.5763566,
+        lng: -75.5763566,
+    },
+    {
+        name: 'Quokka Coworking',
+        lat: 6.2085062,
+        lng: -75.5705925,
+    },
+    {
+        name: 'coworkYA!',
+        lat: 6.2100688,
+        lng: -75.5687043,
+    },
+    {
+        name: 'Global Express Travel & Business Center',
+        lat: 6.2091942,
+        lng: -75.5686613,
+    },
+    {
+        name: 'The Office Coworking',
+        lat: 6.2094521,
+        lng: -75.5705376,
     },
 ];
 
-/*
- * View
- */
- $(function() {
-    $(".button-collapse").sideNav();
- });
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+
+// Define the global scope
+var map;
+
+/*
+ * CoWorking space constructor, this contains and binds a Coworking space's data in one place
+ */
+
+var CoworkingSpace = function (aCoworkingSpace) {
+
+    // A CoWorking Space default data
+    this.name = ko.observable(aCoworkingSpace.name);
+    this.lat = ko.observable(aCoworkingSpace.lat);
+    this.long = ko.observable(aCoworkingSpace.lng);
+    this.marker = new google.maps.Marker({
+        position: {lat: aCoworkingSpace.lat, lng: aCoworkingSpace.lng},
+        text: aCoworkingSpace.name,
+        title: aCoworkingSpace.name,
+        animation: google.maps.Animation.DROP,
+    });
+    
+    // Control the visiblity of a marker
+    this.markerIsVisible = ko.observable(true);
+    this.loadMarker = ko.computed(function() {
+		if(this.markerIsVisible()) {
+            this.marker.setMap(map);
+		} else if (!this.markerIsVisible()){
+            this.marker.setMap(null);
+        } else {
+            return true;
+        }
+    }, this);
+
+    // Add information and animations to a marker when clicked
+    this.marker.addListener('click', function(){
+        console.log('Marker clicked');
+
+	});
+    
+};
+
+
+/*
+ * Define KO's ViewModel
+ */
+
+var viewModel = function() {
+
+    var self = this;
+
+    // Google Maps
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: { lat: 6.2066054, lng: -75.5727518 },
         scrollwheel: false
     });
 
-    
-
-}   
-
-var locationData = function() {
+    // Create an Observable Array to load items into
+    self.locationsArray = ko.observableArray([]);
     for (var i = 0; i < locations.length; i++) {
-        var marker = new google.maps.Marker({
-            position: {lat: locations[i].lat, lng: locations[i].long},
-            map: map,
-            title: 'Hello World!'
-        });
+        self.locationsArray.push(new CoworkingSpace(locations[i]));
     }
-}
+    
+    // Create an observable that holds the text being typed in search bar
+    self.searchFilter = ko.observable("");
 
-var viewModel = function() {
-    var self = this;
+    // Create a Computed Observable to filter items in the filtered list view (HTML)
+    // Filtering an array: http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+    self.searchFilterLocation = ko.computed(function() {
+        var input = self.searchFilter().toLowerCase();
+        var empty = false;
+        // Define missing KO functionality in the minified KO file
+        var stringStartsWith = function (string, startsWith) {          
+            string = string || "";
+            if (startsWith.length > string.length)
+                return false;
+            return string.substring(0, startsWith.length) === startsWith;
+        };
 
-    self.filterLocation = ko.observable("");
+		if (input === empty) {
+            // Show locations
+            self.locationsArray().forEach(function(coworkingSpace){
+				coworkingSpace.markerIsVisible(true);
+			});
+			return self.locationsArray();
+		} else {
+            // Filter locations
+            return ko.utils.arrayFilter(self.locationsArray(), function(coworkingSpace) {
+				var result = stringStartsWith(coworkingSpace.name().toLowerCase(), input);
+				coworkingSpace.markerIsVisible(result);
+				return result;
+			});
+		}
+    }, self);
 
-    var locationsArray = ko.observableArray(locations);
-
-    console.log(locationsArray());
 };
 
-ko.applyBindings(viewModel);
+
+/*
+ * Launch the App!
+ */
+
+function init() {
+    $(".button-collapse").sideNav();
+    ko.applyBindings(viewModel);    
+}
